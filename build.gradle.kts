c674b29a36ac17a103db87792a27496c496653eb
Muhammad Kamal <mohamedkam000@gmail.com>
2025-08-06 01:15:26 +0200

    Update and rename build.gradle to build.gradle.kts
    Switching to kotlin

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("com.android.application") version '8.4.0' apply false
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
