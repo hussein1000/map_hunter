dc27db1940b4814c809ad1dba5765c24ee35fa9b
Hussein <husseinsaleh1000999@gmail.com>
2025-08-05 16:34:28 +0200

    Cleanup

--- app/src/main/java/com/map/hunter/AboutActivity.java
+++ app/src/main/java/com/map/hunter/AboutActivity.java
@@ -1,32 +1,20 @@
 package com.map.hunter;
+
 import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
-
-import android.net.Uri;
 import android.os.Bundle;
-import android.text.SpannableString;
-import android.text.style.ForegroundColorSpan;
-import android.text.style.UnderlineSpan;
 import android.view.MenuItem;
 import android.view.View;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
 
 import com.franmontiel.localechanger.LocaleChanger;
-
-import java.util.List;
-
-
 import com.map.hunter.databinding.ActivityAboutBinding;
 
-
-public class AboutActivity  extends AppCompatActivity {
-
+public class AboutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -43,20 +31,11 @@ public class AboutActivity  extends AppCompatActivity {
         } catch (PackageManager.NameNotFoundException ignored) {}
 
         setTitle(R.string.about_the_app);
-        if( getSupportActionBar() != null) {
+        if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
-
-
-        //Developer Github
-        content = new SpannableString(binding.github.getText().toString());
-        content.setSpan(new UnderlineSpan(), 0, content.length(), 0);
-        binding.github.setText(content);
-        binding.github.setOnClickListener(v -> {
-            Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/hussein1000/map_hunter"));
-            startActivity(browserIntent);
-        });
+    }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
@@ -78,8 +57,4 @@ public class AboutActivity  extends AppCompatActivity {
         newBase = LocaleChanger.configureBaseContext(newBase);
         super.attachBaseContext(newBase);
     }
-
-
-
-
 }
--- app/src/main/java/com/map/hunter/MainApplication.java
+++ app/src/main/java/com/map/hunter/MainApplication.java
@@ -1,47 +1,18 @@
 package com.map.hunter;
 
 import android.app.Application;
-import android.content.SharedPreferences;
 import android.content.res.Configuration;
-
 import androidx.annotation.NonNull;
-
-import com.franmontiel.localechanger.LocaleChanger;
-import java.util.Locale;
-
-import com.map.hunter.helper.Helper;
-
-import static com.map.hunter.helper.Helper.SUPPORTED_LOCALES;
 
 public class MainApplication extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-
-        try {
-            SharedPreferences sharedpreferences = getSharedPreferences(Helper.APP_SHARED_PREF, android.content.Context.MODE_PRIVATE);
-            String defaultLocaleString = sharedpreferences.getString(Helper.SET_DEFAULT_LOCALE_NEW, null);
-            if (defaultLocaleString != null) {
-                Locale defaultLocale;
-                if (defaultLocaleString.equals("zh-CN"))
-                    defaultLocale = Locale.SIMPLIFIED_CHINESE;
-                else if (defaultLocaleString.equals("zh-TW"))
-                    defaultLocale = Locale.TRADITIONAL_CHINESE;
-                else
-                    defaultLocale = new Locale(defaultLocaleString);
-                SUPPORTED_LOCALES.add(defaultLocale);
-            } else {
-                SUPPORTED_LOCALES.add(Locale.getDefault());
-            }
-            LocaleChanger.initialize(getApplicationContext(), SUPPORTED_LOCALES);
-        } catch (Exception ignored) { }
-
     }
 
     @Override
     public void onConfigurationChanged(@NonNull Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-        LocaleChanger.onConfigurationChanged();
     }
 }
