3827a1b41dfe513811fde1c59c7487bb3e0ab133
Hussein Saleh <74200917+hussein1000@users.noreply.github.com>
2025-08-06 09:31:23 +0200

    Update MapHunterWebViewClient.java
    Clean-up

--- app/src/main/java/com/map/hunter/webview/MapHunterWebViewClient.java
+++ app/src/main/java/com/map/hunter/webview/MapHunterWebViewClient.java
@@ -57,79 +57,6 @@ public class MapHunterWebViewClient extends WebViewClient {
             ByteArrayInputStream nothing = new ByteArrayInputStream("".getBytes());
             return new WebResourceResponse("text/javascript", "utf-8", nothing);
         }
-/*        else if(url.contains(Helper.hydrant_contrib_map) && url.endsWith(".js") ){
-            try {
-                URL url_ = new URL(url);
-                URLConnection connection = url_.openConnection();
-                InputStream in = connection.getInputStream();
-                FileOutputStream fos = new FileOutputStream(new File(activity.getCacheDir()+"/tmp.js"));
-                byte[] buf = new byte[512];
-                while (true) {
-                    int len = in.read(buf);
-                    if (len == -1) {
-                        break;
-                    }
-                    fos.write(buf, 0, len);
-                }
-                in.close();
-                fos.flush();
-                fos.close();
-                File file = new File(activity.getCacheDir()+"/tmp.js");
-                FileOutputStream fileinput = new FileOutputStream(file, true);
-                PrintStream printstream = new PrintStream(fileinput);
-                String data = "window.localStorage.setItem(\"welcomeTabShown\", true);\n" +
-                        "window.localStorage.setItem(\"ogdViennaModal\", \""+ System.currentTimeMillis()+"\");\n" +
-                        "window.localStorage.setItem(\"ogdViennaOptIn\", false);";
-                printstream.print(data);
-                fileinput.close();
-                FileInputStream fileInputStream = new FileInputStream(file);
-                return new WebResourceResponse("text/javascript", "utf-8", fileInputStream);
-            } catch (Exception e) {
-                e.printStackTrace();
-                return super.shouldInterceptRequest(view, url);
-            }
-        }
-        else if(url.contains(Helper.ben_map) &&  url.contains("main") && url.endsWith(".js") ){
-            try {
-                URL url_ = new URL(url);
-                URLConnection connection = url_.openConnection();
-                InputStream in = connection.getInputStream();
-                FileOutputStream fos = new FileOutputStream(new File(activity.getCacheDir()+"/tmp2.js"));
-                byte[] buf = new byte[512];
-                while (true) {
-                    int len = in.read(buf);
-                    if (len == -1) {
-                        break;
-                    }
-                    fos.write(buf, 0, len);
-                }
-                in.close();
-                fos.flush();
-                fos.close();
-                String content;
-                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.KITKAT) {
-                    content = IOUtils.toString(new FileInputStream(activity.getCacheDir()+"/tmp2.js"), StandardCharsets.UTF_8);
-                }else{
-                    content = IOUtils.toString(new FileInputStream(activity.getCacheDir()+"/tmp2.js"), "utf-8");
-                }
-                content = content.replaceAll("window.innerWidth>360", "window.innerWidth>0");
-                IOUtils.write(content, new FileOutputStream(activity.getCacheDir()+"/tmp2.js"), "utf-8");
-                FileInputStream fileInputStream = new FileInputStream(activity.getCacheDir()+"/tmp2.js");
-                return new WebResourceResponse("text/javascript", "utf-8", fileInputStream);
-            } catch (Exception e) {
-                e.printStackTrace();
-                return super.shouldInterceptRequest(view, url);
-            }
-        }
-        else if( Build.VERSION.SDK_INT <= Build.VERSION_CODES.LOLLIPOP_MR1 && url.compareTo("https://www.mapcontrib.xyz/js/commons.bundle.js") == 0){
-            InputStream commonJS = null;
-            try {
-                commonJS = activity.getAssets().open("js/commons.bundle.js");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            return new WebResourceResponse("text/javascript", "utf-8", commonJS);
-        }*/
         else{
             return super.shouldInterceptRequest(view, url);
         }
@@ -137,8 +64,6 @@ public class MapHunterWebViewClient extends WebViewClient {
 
     @Override
     public void onPageStarted(WebView view, String url, Bitmap favicon) {
-
-
         super.onPageStarted(view, url, favicon);
         String title = Helper.getTitle(activity, url);
         activity.setTitle(title);
@@ -150,36 +75,15 @@ public class MapHunterWebViewClient extends WebViewClient {
             MainActivity.canGoBack = false;
         }
 
-/*        if( url.contains(Helper.ben_map)) {
-            if (activity instanceof MainActivity) {
-                Location currentLocation = ((MainActivity) activity).getCurrentLocation();
-                if (currentLocation != null) {
-                    String coord = "{\"app\":{\"mapCoords\":[" + currentLocation.getLongitude() + "," + currentLocation.getLatitude() + ",10]}}";
-                    view.loadUrl("javascript:window.localStorage.setItem(\"persistedState\", '" + coord + "')");
-                }else{
-                    SharedPreferences sharedpref = activity.getSharedPreferences(Helper.APP_SHARED_PREF, MODE_PRIVATE);
-                    String location = sharedpref.getString(Helper.LAST_LOCATION, null);
-                    if( location != null && location.split(",").length > 1){
-                        String longitude =  location.split(",")[0];
-                        String latitude = location.split(",")[1];
-                        String coord = "{\"app\":{\"mapCoords\":[" + longitude + "," + latitude+ ",10]}}";
-                        view.loadUrl("javascript:window.localStorage.setItem(\"persistedState\", '" + coord + "')");
-                    }
-                }
-            }
-        }*/
         if (activity instanceof MainActivity) {
             ((MainActivity) activity).showProgressDialog();
         }
     }
 
-
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onReceivedError(WebView view, WebResourceRequest request,
                                 WebResourceError error) {
-
-
         if (!isConnected()) {
             final Snackbar snackBar = Snackbar.make(rootView, activity.getString(R.string.no_internet), Snackbar.LENGTH_INDEFINITE);
             snackBar.setAction(activity.getString(R.string.enable_data), v -> {
@@ -191,14 +95,12 @@ public class MapHunterWebViewClient extends WebViewClient {
         }
 
         super.onReceivedError(view, request, error);
-
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     @Override
     public void onReceivedHttpError(WebView view,
                                     WebResourceRequest request, WebResourceResponse errorResponse) {
-
         if (!isConnected()) {
             final Snackbar snackBar = Snackbar.make(rootView, activity.getString(R.string.no_internet), Snackbar.LENGTH_INDEFINITE);
             snackBar.setAction(activity.getString(R.string.enable_data), v -> {
@@ -225,10 +127,6 @@ public class MapHunterWebViewClient extends WebViewClient {
     @Override
     public void onPageFinished(WebView view, String url) {
         Helper.recordLocationFromUrl(activity,  url);
-/*        if (url.contains(Helper.gribrouillon) ){
-            Helper.injectCSS(activity, view, "css/gribouillon.css");
-        }*/
-
         ((MainActivity)activity).hideProgressDialog();
     }
 }
