1a785ee16595baa152877c7cd661937aa7cfd8ad
Hussein Saleh <74200917+hussein1000@users.noreply.github.com>
2025-08-06 09:23:48 +0200

    Update AboutActivity.java
    Clean-up

--- app/src/main/java/com/map/hunter/AboutActivity.java
+++ app/src/main/java/com/map/hunter/AboutActivity.java
@@ -1,31 +1,8 @@
 package com.map.hunter;
-
-/*import android.content.Context;
-import android.content.Intent;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.res.Configuration;
-
-import android.net.Uri;*/
 import android.os.Bundle;
-/*import android.text.SpannableString;
-import android.text.style.ForegroundColorSpan;
-import android.text.style.UnderlineSpan;
-import android.view.MenuItem;*/
 import android.view.View;
-//import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-/*import androidx.core.content.ContextCompat;
-import androidx.recyclerview.widget.LinearLayoutManager;
-
-import com.franmontiel.localechanger.LocaleChanger;
-import java.util.List;*/
-
 import com.map.hunter.databinding.ActivityAboutBinding;
-/*import com.map.hunter.drawer.ContributorsDrawer;
-import com.map.hunter.entity.Contributor;
-import com.map.hunter.helper.ContributorsData;*/
-
 
 public class AboutActivity  extends AppCompatActivity {
 
@@ -43,25 +20,4 @@ public class AboutActivity  extends AppCompatActivity {
             getSupportActionBar().setDisplayShowHomeEnabled(true);
         }
     }
-
-/*    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == android.R.id.home) {
-            finish();
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onConfigurationChanged(@NonNull Configuration newConfig) {
-        super.onConfigurationChanged(newConfig);
-        LocaleChanger.onConfigurationChanged();
-    }
-
-    @Override
-    protected void attachBaseContext(Context newBase) {
-        newBase = LocaleChanger.configureBaseContext(newBase);
-        super.attachBaseContext(newBase);
-    }*/
 }
